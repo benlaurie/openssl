affcf937fb4bb092c9cd7d1c3eeadd022914ad3a
Ben Laurie <ben@links.org>
2016-03-29 19:33:26 +0100

    Reproduce needs to use the right size buffer for asan to work.

--- fuzz/reproduce.c
+++ fuzz/reproduce.c
@@ -1,10 +1,17 @@
+#include <assert.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 int main(int argc, char **argv) {
+    struct stat st;
+    stat(argv[1], &st);
     FILE *f = fopen(argv[1], "r");
-    char buf[4096];
-    size_t s = fread(buf, 1, sizeof buf, f);
+    char *buf = malloc(st.st_size);
+    size_t s = fread(buf, 1, st.st_size, f);
+    assert(s == st.st_size);
     LLVMFuzzerTestOneInput(buf, s);
+    free(buf);
+    fclose(f);
     return 0;
 }
 
