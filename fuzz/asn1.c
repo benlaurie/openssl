2769b6d174178f7779a1924ce3ef3c7f6e8abc29
Ben Laurie <ben@links.org>
2016-04-04 11:50:41 +0100

    Add more types.

--- fuzz/asn1.c
+++ fuzz/asn1.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <openssl/asn1.h>
 #include <openssl/asn1t.h>
+#include <openssl/ec.h>
+#include <openssl/pkcs12.h>
 #include <openssl/x509v3.h>
 #include <FuzzerInterface.h>
-
-// Change GENERAL_NAME to whatever you want to fuzz
 
 static const ASN1_ITEM *item_type;
 
@@ -22,8 +22,24 @@ int LLVMFuzzerInitialize(int *argc, char ***argv) {
         item_type = ASN1_ITEM_rptr(ASN1_SEQUENCE);
     else if(!strcmp(cmd, "BIGNUM"))
         item_type = ASN1_ITEM_rptr(BIGNUM);
+    else if(!strcmp(cmd, "ECPARAMETERS"))
+        item_type = ASN1_ITEM_rptr(ECPARAMETERS);
+    else if(!strcmp(cmd, "ECPKPARAMETERS"))
+        item_type = ASN1_ITEM_rptr(ECPKPARAMETERS);
     else if (!strcmp(cmd, "GENERAL_NAME"))
         item_type = ASN1_ITEM_rptr(GENERAL_NAME);
+    else if (!strcmp(cmd, "GENERAL_SUBTREE"))
+        item_type = ASN1_ITEM_rptr(GENERAL_SUBTREE);
+    else if (!strcmp(cmd, "NAME_CONSTRAINTS"))
+        item_type = ASN1_ITEM_rptr(NAME_CONSTRAINTS);
+    else if (!strcmp(cmd, "PKCS12_AUTHSAFES"))
+        item_type = ASN1_ITEM_rptr(PKCS12_AUTHSAFES);
+    else if (!strcmp(cmd, "PKCS12_SAFEBAGS"))
+        item_type = ASN1_ITEM_rptr(PKCS12_SAFEBAGS);
+    else if (!strcmp(cmd, "POLICY_CONSTRAINTS"))
+        item_type = ASN1_ITEM_rptr(POLICY_CONSTRAINTS);
+    else if (!strcmp(cmd, "POLICY_MAPPINGS"))
+        item_type = ASN1_ITEM_rptr(POLICY_MAPPINGS);
     else if(!strcmp(cmd, "X509"))
         item_type = ASN1_ITEM_rptr(X509);
     else
